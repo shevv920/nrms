df5e0482e031b3d16df43068b8a548d3419617a5
Uladzimir Shautsou <shevv920@proton.me>
2023-05-13 12:51:38 +0200

    WIP

---
+++ apps/api/src/types.ts
@@ -0,0 +1,6 @@
+export type { Config } from '~/Modules/Config';
+export type { Logger } from '~/Modules/Logger';
+export type { HttpApp } from '~/Modules/App/App';
+export type { Middlewares } from '~/Modules/Middlewares/live';
+export type { PrivateRoutes } from '~/Modules/Routes/private/live';
+export type { PublicRoutes } from '~/Modules/Routes/public/live';
